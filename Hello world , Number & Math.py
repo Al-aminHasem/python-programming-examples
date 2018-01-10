f6c4446e4b14b009741f935a063538c4cb015d5e
AL-AMIN <32561876+Alamin999bd@users.noreply.github.com>
2018-01-10 19:59:03 +0600

    Add files via upload

---
+++ Hello world , Number & Math.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("I'm AL-AMIN")
+print('Hello World')
+print('I am AL-AMIN')
+print(2+3)
+print(10-5)
+print(10*5)
+print(10/2)
+print(10/3)
+print(10%3)
+print(2**3)
+a=2
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+a="al-amin"
+b="Student of JKKNIU"
+c=a+b
+print(c)
+
